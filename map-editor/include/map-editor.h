ae9f3a93e63d34ac8bcbe42aaeb311134bd882e8
Anton KRIVOSHEYA <akrivosh@e1r9p8.unit.ua>
2019-09-07 18:18:21 +0300

    map editor

--- map-editor/include/map-editor.h
+++ map-editor/include/map-editor.h
@@ -6,10 +6,46 @@
 /*   By: akrivosh <akrivosh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/07 17:35:57 by akrivosh          #+#    #+#             */
-/*   Updated: 2019/09/07 17:36:49 by akrivosh         ###   ########.fr       */
+/*   Updated: 2019/09/07 18:13:57 by akrivosh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <SDL.h>
 # include <SDL_mixer.h>
 # include <SDL_ttf.h>
+
+# define SDL		m->sdl
+
+# define WIN_W		1500
+# define WIN_H		1000
+
+# define WIN_FLG	SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
+
+# define GRAY		0x808080
+
+typedef struct s_sdl		t_sdl;
+typedef struct s_main		t_main;
+
+struct				s_sdl
+{
+	SDL_Window		*win;
+	SDL_Surface		*sur;
+	SDL_DisplayMode	display;
+	SDL_Event		event;
+	TTF_Font		*ttf;
+	SDL_Color		white;
+	SDL_Color		yellow;
+};
+
+struct				s_main
+{
+	t_sdl			sdl;
+	int				map_tab;
+};
+
+void				init_sdl(t_main *m);
+void				loop(t_main *m);
+void				map_editor(t_main *m);
+void				draw_grid(t_main *m);
+void				map_tabs(t_main *m);
+void				ft_put_pixel(t_main *m, int x, int y, int pixel);
---
+++ map-editor/source/main.c
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akrivosh <akrivosh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/07 17:43:39 by akrivosh          #+#    #+#             */
+/*   Updated: 2019/09/07 18:15:32 by akrivosh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "map-editor.h"
+
+int		main(void)
+{
+	t_main	m;
+
+	init_sdl(&m);
+	loop(&m);
+	return (0);
+}
+
+void	init_sdl(t_main *m)
+{
+	int		request;
+
+	if (SDL_Init(SDL_INIT_EVERYTHING))
+		exit(EXIT_FAILURE);
+	if (TTF_Init())
+		exit(EXIT_FAILURE);
+	request = SDL_GetDesktopDisplayMode(0, &SDL.display);
+	if (!(SDL.win = SDL_CreateWindow("Doom 2077 [map editor mode]",
+	SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIN_W, WIN_H, WIN_FLG)))
+		exit(EXIT_FAILURE);
+	if (!(SDL.sur = SDL_GetWindowSurface(SDL.win)))
+		exit(EXIT_FAILURE);
+	if (!(SDL.ttf = TTF_OpenFont("../resources/font/SAMBD.ttf", 28)))
+		exit(EXIT_FAILURE);
+	SDL_CaptureMouse(SDL_TRUE);
+	SDL.white.r = 255;
+	SDL.white.g = 255;
+	SDL.white.b = 255;
+	SDL.yellow.r = 255;
+	SDL.yellow.g = 255;
+	SDL.yellow.b = 0;
+}
+
+void	loop(t_main *m)
+{
+	while (1)
+	{
+		SDL_FillRect(SDL.sur, NULL, 0x000000);
+		map_editor(m);
+		while (SDL_PollEvent(&SDL.event))
+			if ((SDL.event.type == SDL_QUIT) ||
+			(SDL.event.key.type == SDL_KEYDOWN
+			&& SDL.event.key.keysym.sym == SDLK_ESCAPE))
+				exit(EXIT_SUCCESS);
+		SDL_UpdateWindowSurface(SDL.win);
+	}
+}
+
+void	map_editor(t_main *m)
+{
+	draw_grid(m);
+	map_tabs(m);
+}
+
+void	draw_grid(t_main *m)
+{
+	int		x;
+	int		y;
+
+	x = 250;
+	while (++x < (int)WIN_W - 1)
+	{
+		y = 0;
+		while (++y < (int)WIN_H - 1)
+		{
+			if (!(y % 10) || !(x % 10))
+				ft_put_pixel(m, x, y, GRAY);
+			if (!(y % 100) || !(x % 100))
+			{
+				ft_put_pixel(m, x + 1, y, GRAY);
+				ft_put_pixel(m, x - 1, y, GRAY);
+				ft_put_pixel(m, x, y + 1, GRAY);
+				ft_put_pixel(m, x, y - 1, GRAY);
+			}
+		}
+	}
+}
+
+void	map_tabs(t_main *m)
+{
+	SDL_Surface		*sur;
+	SDL_Rect		rect;
+
+	rect.w = 0;
+	rect.h = 0;
+	rect.y = 10;
+	rect.x = 10;
+	sur = TTF_RenderText_Solid(SDL.ttf, "Sector", (m->map_tab) ? SDL.white : SDL.yellow);
+	SDL_BlitSurface(sur, NULL, SDL.sur, &rect);
+	SDL_FreeSurface(sur);
+	if (SDL.event.type == SDL_MOUSEBUTTONDOWN
+	&& SDL.event.button.button == SDL_BUTTON_LEFT
+	&& SDL.event.button.x >= rect.x
+	&& SDL.event.button.x <= (rect.x + rect.w)
+	&& SDL.event.button.y >= rect.y
+	&& SDL.event.button.y <= (rect.x + rect.h))
+		m->map_tab = 0;
+	rect.x += 110;
+	sur = TTF_RenderText_Solid(SDL.ttf, "Objects", (m->map_tab) ? SDL.yellow : SDL.white);
+	SDL_BlitSurface(sur, NULL, SDL.sur, &rect);
+	SDL_FreeSurface(sur);
+	if (SDL.event.type == SDL_MOUSEBUTTONDOWN
+	&& SDL.event.button.button == SDL_BUTTON_LEFT
+	&& SDL.event.button.x >= rect.x
+	&& SDL.event.button.x <= (rect.x + rect.w)
+	&& SDL.event.button.y >= rect.y
+	&& SDL.event.button.y <= (rect.x + rect.h))
+		m->map_tab = 1;
+}
+
+void	ft_put_pixel(t_main *m, int x, int y, int pixel)
+{
+	int		*new_pixel;
+
+	if (x < 0 || x > SDL.display.w || y < 0 || y > SDL.display.h - 2)
+		return ;
+	new_pixel = SDL.sur->pixels + y * SDL.sur->pitch
+	+ x * SDL.sur->format->BytesPerPixel;
+	*new_pixel = pixel;
+}
