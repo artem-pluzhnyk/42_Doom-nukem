257957c6555a9ee6f004eb3c7b7ccde0115d2026
Artem Pluzhnik <black.nphlm@pm.me>
2019-09-10 14:37:33 +0300

    death text

--- engine/source/windows/endgame.c
+++ engine/source/windows/endgame.c
@@ -6,7 +6,7 @@
 /*   By: apluzhni <apluzhni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 19:00:57 by apluzhni          #+#    #+#             */
-/*   Updated: 2019/09/09 19:20:39 by apluzhni         ###   ########.fr       */
+/*   Updated: 2019/09/10 14:31:10 by apluzhni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 void	endgame(t_main *m)
 {
+	SDL_Surface		*sur;
+	SDL_Rect		rect;
+
+	rect.x = WIN_W / 5;
+	rect.y = WIN_H / 22;
+	rect.w = 0;
+	rect.h = 0;
 	draw_background(m, SDL.texture[16]);
+	sur = TTF_RenderText_Solid(SDL.ttf_28,"Atque in pepetuum, frater, ave atque vale.", SDL.red);
+	SDL_BlitSurface(sur, NULL, SDL.sur, &rect);
+	rect.y += 30;
+	sur = TTF_RenderText_Solid(SDL.ttf_28,"Forever and ever, my brother, hail and farewell.", SDL.red);
+	SDL_BlitSurface(sur, NULL, SDL.sur, &rect);
+	SDL_FreeSurface(sur);
 }
--- engine/source/windows/loading_screen.c
+++ engine/source/windows/loading_screen.c
@@ -6,7 +6,7 @@
 /*   By: apluzhni <apluzhni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/05 19:40:31 by apluzhni          #+#    #+#             */
-/*   Updated: 2019/09/10 12:47:36 by apluzhni         ###   ########.fr       */
+/*   Updated: 2019/09/10 14:29:50 by apluzhni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,10 @@ void	loading_text(t_main *m)
 	rect.y = WIN_H - WIN_H / 8;
 	rect.w = 0;
 	rect.h = 0;
-	sur = TTF_RenderText_Solid(SDL.ttf_50,"Doom 2077 is loaded", SDL.red);
+	sur = TTF_RenderText_Solid(SDL.ttf_50,"Doom 2077 is loaded", SDL.blue);
 	SDL_BlitSurface(sur, NULL, SDL.sur, &rect);
 	rect.y += 30;
-	sur = TTF_RenderText_Solid(SDL.ttf_50,"Press Enter to continue", SDL.red);
+	sur = TTF_RenderText_Solid(SDL.ttf_50,"Press Enter to continue", SDL.blue);
 	SDL_BlitSurface(sur, NULL, SDL.sur, &rect);
 	SDL_FreeSurface(sur);
 }
