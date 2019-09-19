ae4d456fc2541be4c7d6f7dd16d21ce9488cd345
Artem Pluzhnik <black.nphlm@pm.me>
2019-09-19 19:46:24 +0300

    remove libzip

--- libzip/build/man/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/apluzhni/Desktop/doom-nukem/libzip/man
-# Build directory: /Users/apluzhni/Desktop/doom-nukem/libzip/build/man
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
