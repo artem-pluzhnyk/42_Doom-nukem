a4241f2b94b2c9102babba5fd8a2f18fb35a3765
Artem Pluzhnik <black.nphlm@pm.me>
2019-09-19 14:48:26 +0300

    cmake

---
+++ libzip/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /Users/apluzhni/Desktop/doom-nukem/libzip
+# Build directory: /Users/apluzhni/Desktop/doom-nukem/libzip/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib")
+subdirs("man")
+subdirs("src")
+subdirs("regress")
+subdirs("examples")
---
+++ libzip/build/src/CMakeFiles/zipcmp.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/apluzhni/Desktop/doom-nukem/libzip/src/zipcmp.c" "/Users/apluzhni/Desktop/doom-nukem/libzip/build/src/CMakeFiles/zipcmp.dir/zipcmp.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src/.."
+  "../src/../lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/apluzhni/Desktop/doom-nukem/libzip/build/lib/CMakeFiles/zip.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
